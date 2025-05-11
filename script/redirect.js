f8c6dceafcf4fb5be51e4aada5abbd6433bfc7c0
masumhs73 <masumshuvo58@gmail.com>
2025-05-11 08:30:43 +0600

    fixed redirect issues

---
+++ script/redirect.js
@@ -0,0 +1,5 @@
+document.getElementById('discover')
+.addEventListener('click', function(event){
+    event.preventDefault();
+    window.location.href = "./second.html";
+})
---
+++ script/second.js
@@ -0,0 +1,7 @@
+
+document.getElementById('btn-back')
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+        // console.log("yess");
+        window.location.href = "./index.html";
+    })
