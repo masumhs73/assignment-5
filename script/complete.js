0de1da9aa60c6f3c10e8ef5f6fe8d3ae47dd7497
masumhs73 <masumshuvo58@gmail.com>
2025-05-12 18:39:50 +0600

    fixed issues

--- script/complete.js
+++ script/complete.js
@@ -2,9 +2,10 @@ const devNumber = document.getElementById('dev-number').innerText;
 const convertedDevNumber = parseInt(devNumber);
 const taskNumber = document.getElementById('task-number').innerText;
 const convertedTaskNumber = parseInt(taskNumber);
-// console.log(taskAssign);
+
 let devCount = 0;
 let taskCount = 0;
+
 const buttons = document.querySelectorAll('.btnComplete');
 for (const btnComplete of buttons) {
     btnComplete.addEventListener('click', function (event) {
@@ -13,12 +14,10 @@ for (const btnComplete of buttons) {
         btnComplete.disabled = true;
         devCount++;
         const updatedDevNumber = convertedDevNumber + devCount;
-        // console.log(updatedDevCount);
         document.getElementById('dev-number').innerText = updatedDevNumber;
 
         taskCount++;
         const updatedTaskNumber = convertedTaskNumber - taskCount;
-        // console.log(updatedTaskNumber);
 
         document.getElementById('task-number').innerText = updatedTaskNumber;
         if (updatedTaskNumber === 0) {
@@ -29,7 +28,6 @@ for (const btnComplete of buttons) {
         const div = document.createElement('div');
         div.classList.add("bg-sky-50", "m-3", "p-2");
 
-        // const title = button.dataset.title;
         let now = new Date();
         let currentTime = now.toLocaleTimeString({ hour: '2-digit', minute: '2-digit', second: '2-digit' });
 
@@ -40,10 +38,3 @@ for (const btnComplete of buttons) {
     })
 
 }
-
-document.getElementById('btn-clear')
-    .addEventListener('click', function (event) {
-        event.preventDefault();
-        
-        div.innerHTML.remove();
-    })
